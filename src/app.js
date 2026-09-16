e1e8fdc1e3ef7521635f81e984ef2a8c4d3c6374
trtramnh <minhtruong300605@gmail.com>
2026-09-16 12:12:28 +0700

    feat: complete authentication and role authorization

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+
+const testRoute = require("./routes/test.route");
 const authRoute = require("./routes/auth.route");
 
 const app = express();
+
 
 app.use(express.json());
 
@@ -12,5 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
+
+
 
 module.exports = app;
